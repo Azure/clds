521c01b6b078512679281c6332140001d8648c0f
Andrei Porumb <anporumb@microsoft.com>
2019-03-08 13:39:00 -0800

    fix main.c

--- tests/clds_hash_table_ut/main.c
+++ tests/clds_hash_table_ut/main.c
@@ -8,7 +8,7 @@ int main(void)
     RUN_TEST_SUITE(clds_hash_table_unittests, failedTestCount);
 
 #ifdef VLD_OPT_REPORT_TO_STDOUT
-    failedTestCount = (failedTestCount>0)?failedTestCount:-(int)VLDGetLeaksCount() > 0 ? 1 : 0;
+    failedTestCount = (failedTestCount>0)?failedTestCount:-(int)VLDGetLeaksCount();
 #endif
 
     return failedTestCount;
