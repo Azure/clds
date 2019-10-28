5016ad79fb1a73a0168172323c26a199db420aed
Dan Cristoloveanu <dcristo@microsoft.com>
2019-10-28 15:38:16 -0700

    Use PRI_MU_ENUM and update dependencies

--- tests/lock_free_set_ut/lock_free_set_ut.c
+++ tests/lock_free_set_ut/lock_free_set_ut.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #endif
 
+#include "azure_macro_utils/macro_utils.h"
 #include "testrunnerswitcher.h"
 
 void* real_malloc(size_t size)
@@ -35,7 +36,7 @@ MU_DEFINE_ENUM_STRINGS(UMOCK_C_ERROR_CODE, UMOCK_C_ERROR_CODE_VALUES)
 
 static void on_umock_c_error(UMOCK_C_ERROR_CODE error_code)
 {
-    ASSERT_FAIL("umock_c reported error :%s", MU_ENUM_TO_STRING(UMOCK_C_ERROR_CODE, error_code));
+    ASSERT_FAIL("umock_c reported error :%" PRI_MU_ENUM "", MU_ENUM_VALUE(UMOCK_C_ERROR_CODE, error_code));
 }
 
 MOCK_FUNCTION_WITH_CODE(, void, test_node_cleanup, void*, context, struct LOCK_FREE_SET_ITEM_TAG*, item)
