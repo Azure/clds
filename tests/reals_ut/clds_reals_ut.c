69486ef5a65dafac9c96e21cb9a65c641d473cff
Dan Cristoloveanu <dcristo@microsoft.com>
2020-11-09 09:02:39 -0800

    Add mpsc_lock_free_queue reals check in reals_ut (#25)
    * Add mpsc_lock_free_queue reals check in real_ut
    
    * Add usage of real interlocked
    
    * Add reals files to reals CMakeLists.txt for Linux also

--- tests/reals_ut/clds_reals_ut.c
+++ tests/reals_ut/clds_reals_ut.c
@@ -6,43 +6,40 @@
 
 #define ENABLE_MOCKS
 
-#if defined _MSC_VER
 #include "clds/clds_hazard_pointers.h"
 #include "clds/clds_hash_table.h"
 #include "clds/clds_singly_linked_list.h"
 #include "clds/clds_sorted_list.h"
 #include "clds/clds_st_hash_set.h"
-#endif
 #include "clds/lock_free_set.h"
+#include "clds/mpsc_lock_free_queue.h"
 
 #undef ENABLE_MOCKS
 
-#if defined _MSC_VER
 #include "../tests/reals/real_clds_hazard_pointers.h"
 #include "../tests/reals/real_clds_hash_table.h"
 #include "../tests/reals/real_clds_singly_linked_list.h"
 #include "../tests/reals/real_clds_sorted_list.h"
 #include "../tests/reals/real_clds_st_hash_set.h"
-#endif
 #include "../tests/reals/real_lock_free_set.h"
+#include "../tests/reals/real_mpsc_lock_free_queue.h"
 
 BEGIN_TEST_SUITE(clds_reals_ut)
 
 // this test makes sure that the mappings work
 // (there is a real_ function corresponding to the original)
-TEST_FUNCTION(check_all_c_util_reals)
+TEST_FUNCTION(check_all_clds_reals)
 {
     // arrange
 
     // act
-#if defined _MSC_VER
     REGISTER_CLDS_HAZARD_POINTERS_GLOBAL_MOCK_HOOKS();
     REGISTER_CLDS_HASH_TABLE_GLOBAL_MOCK_HOOKS();
     REGISTER_CLDS_SINGLY_LINKED_LIST_GLOBAL_MOCK_HOOKS();
     REGISTER_CLDS_SORTED_LIST_GLOBAL_MOCK_HOOKS();
     REGISTER_CLDS_ST_HASH_SET_GLOBAL_MOCK_HOOKS();
-#endif
     REGISTER_LOCK_FREE_SET_GLOBAL_MOCK_HOOKS();
+    REGISTER_MPSC_LOCK_FREE_QUEUE_GLOBAL_MOCK_HOOKS();
 
     // assert
     // no explicit assert, if it builds it works
