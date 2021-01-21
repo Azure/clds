00042cd0b06596366f9ff52d883a48ba573d23d2
Dan Cristoloveanu <dcristo@microsoft.com>
2021-01-21 15:48:48 -0800

    Fix real files for iwyu

--- tests/reals/real_clds_sorted_list.c
+++ tests/reals/real_clds_sorted_list.c
@@ -1,9 +1,9 @@
 // Licensed under the MIT license.See LICENSE file in the project root for full license information.
 
-#include "real_gballoc_hl_renames.h"
-#include "real_sync_renames.h"
-#include "real_interlocked_renames.h"
+#include "real_gballoc_hl_renames.h" // IWYU pragma: keep
+#include "real_sync_renames.h" // IWYU pragma: keep
+#include "real_interlocked_renames.h" // IWYU pragma: keep
 
-#include "real_clds_sorted_list_renames.h"
+#include "real_clds_sorted_list_renames.h" // IWYU pragma: keep
 
 #include "../src/clds_sorted_list.c"
--- tests/reals/real_mpsc_lock_free_queue.c
+++ tests/reals/real_mpsc_lock_free_queue.c
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-#include "real_gballoc_hl_renames.h"
-#include "real_interlocked_renames.h"
+#include "real_gballoc_hl_renames.h" // IWYU pragma: keep
+#include "real_interlocked_renames.h" // IWYU pragma: keep
 
-#include "real_mpsc_lock_free_queue_renames.h"
+#include "real_mpsc_lock_free_queue_renames.h" // IWYU pragma: keep
 
 #include "../../src/mpsc_lock_free_queue.c"
