fed379219976cc84b2db1ad6f64a3af84598b9d3
Dan Cristoloveanu <dcristo@microsoft.com>
2021-01-21 13:46:07 -0800

    Fix includes for includecheck leg

--- tests/clds_hash_table_perf/main.c
+++ tests/clds_hash_table_perf/main.c
@@ -1,6 +1,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdio.h>
 #include "clds_hash_table_perf.h"
 
 int main(void)
--- tests/clds_singly_linked_list_perf/main.c
+++ tests/clds_singly_linked_list_perf/main.c
@@ -1,6 +1,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdio.h>
 #include "clds_singly_linked_list_perf.h"
 
 int main(void)
