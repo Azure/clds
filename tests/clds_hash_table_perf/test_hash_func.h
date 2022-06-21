5c680d0f57e46af27e36cca2932b3e07856827d4
Andrei Porumb <anporumb@microsoft.com>
2022-06-21 15:39:49 -0700

    not all C++ can die so easily

--- tests/clds_hash_table_perf/test_hash_func.h
+++ tests/clds_hash_table_perf/test_hash_func.h
@@ -3,11 +3,21 @@
 #ifndef TEST_HASH_FUNC_H
 #define TEST_HASH_FUNC_H
 
+#ifdef __cplusplus
+#include <cstdint>
+#else
 #include <stdint.h>
+#endif
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
     
 uint64_t test_compute_hash(void* key);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* TEST_HASH_FUNC_H */
