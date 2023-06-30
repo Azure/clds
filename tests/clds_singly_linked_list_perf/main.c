1fc1925ddb64449e5a139fb258907bdd923aac4a
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-30 13:37:26 -0700

    Fail build when perf tests fail

--- tests/clds_singly_linked_list_perf/main.c
+++ tests/clds_singly_linked_list_perf/main.c
@@ -1,10 +1,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdio.h>
+
+#include "c_logging/logger.h"
+
 #include "clds_singly_linked_list_perf.h"
 
 int main(void)
 {
+    //(void)logger_init();
+
     clds_singly_linked_list_perf_main();
+
+    //logger_deinit();
+
     return 0;
 }
