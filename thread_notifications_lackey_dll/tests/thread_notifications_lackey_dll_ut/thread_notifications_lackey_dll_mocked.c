eec5672c7ded9d9ad8a824ac73ec95c5b2331d2c
Dan Cristoloveanu <dcristo@microsoft.com>
2023-10-21 17:04:06 -0700

    Add UTs and int tests for thread_notifications_lackey_dll (#196)
    * Add UTs and int tests for thread_notifications_lackey_dll
    
    * Fix warning
    
    * Have DllMain_UnderTest
    
    * Fix the failing UT
    
    * Address CR comments
    
    * Fix too long paths

---
+++ thread_notifications_lackey_dll/tests/thread_notifications_lackey_dll_ut/thread_notifications_lackey_dll_mocked.c
@@ -0,0 +1,11 @@
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define logger_init mock_logger_init
+#define logger_deinit mock_logger_deinit
+
+// Since we mightbe running in an environment where a DllMain exists (like running under VS CppUnittest)
+// we want to rename it to something else
+#define DllMain DllMain_under_test
+
+#include "../../src/thread_notifications_lackey_dll.c"
+
