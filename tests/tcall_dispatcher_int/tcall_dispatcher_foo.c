3d1fcd08ec1eeae9509f1e14fe2ecd491440075f
Dan Cristoloveanu <dcristo@microsoft.com>
2023-10-24 07:35:09 -0700

    Add tcall_dispatcher unit tests and int tests (#199)
    * Add tcall_dispatcher unit tests and int tests
    
    * Minor changes after self review
    
    * Add headers with the code
    
    * Fix volatile
    
    * Fix volatile
    
    * Only build tcall_dispatcher_ut on Windows for now
    
    * Appease valgrind/helgrind
    
    * Address CR comments
    
    * Fix int test

---
+++ tests/tcall_dispatcher_int/tcall_dispatcher_foo.c
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+#include <stdint.h>
+
+#include "c_pal/thandle.h"
+
+#include "c_pal/gballoc_hl.h"
+#include "c_pal/gballoc_hl_redirect.h"
+
+#include "clds/tcall_dispatcher.h"
+
+#include "tcall_dispatcher_foo.h"
+
+// This is the call dispatcher used for most of the tests
+THANDLE_TYPE_DEFINE(TCALL_DISPATCHER_TYPEDEF_NAME(FOO));
+TCALL_DISPATCHER_TYPE_DEFINE(FOO, int32_t, x);
