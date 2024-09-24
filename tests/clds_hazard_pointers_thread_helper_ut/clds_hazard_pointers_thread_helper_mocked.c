f829766009e278b914778e95033d56b4f1e5da5a
anporumb <anporumb@users.noreply.github.com>
2024-09-24 10:29:01 -0700

    Anporumb/pchmock (#279)
    * propagate anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * fix clds for linker errors
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * propagate_all_repos anporumb/pchmock
    
    * use latest submodules

--- tests/clds_hazard_pointers_thread_helper_ut/clds_hazard_pointers_thread_helper_mocked.c
+++ tests/clds_hazard_pointers_thread_helper_ut/clds_hazard_pointers_thread_helper_mocked.c
@@ -1,9 +1,26 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license.See LICENSE file in the project root for full license information.
 
+#include "windows.h"
+
 #define TlsAlloc mocked_TlsAlloc
 #define TlsFree mocked_TlsFree
 #define TlsGetValue mocked_TlsGetValue
 #define TlsSetValue mocked_TlsSetValue
+
+extern LPVOID TlsGetValue(
+    DWORD dwTlsIndex
+);
+
+extern BOOL TlsSetValue(
+    DWORD  dwTlsIndex,
+    LPVOID lpTlsValue
+);
+
+extern DWORD TlsAlloc();
+
+extern BOOL TlsFree(
+    DWORD dwTlsIndex
+);
 
 #include "../../src/clds_hazard_pointers_thread_helper.c"
